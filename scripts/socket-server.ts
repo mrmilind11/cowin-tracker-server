2e9fa7d1996218697f3f219d31687ae2753b4e73
Milind Sharma <milind.sharma@searce.com>
2021-05-09 21:57:40 +0530

    CORS url added

--- scripts/socket-server.ts
+++ scripts/socket-server.ts
@@ -17,17 +17,19 @@ let io: Server;
 export const initSocket = (server: http.Server) => {
     io = new Server(server, {
         cors: {
-            origin: ['http://localhost:3000']
+            origin: ['http://localhost:3000', 'https://cowin-tracker-frontend.vercel.app/']
         }
     });
 
     io.on('connection', (socket: Socket) => {
         console.log('Connected to client:', socket.id);
         clientSet.add(socket.id);
+        console.log('Total clients', clientSet);
 
         socket.on('disconnect', () => {
             clientSet.delete(socket.id);
-            console.log('disconnected');
+            console.log('disconnected', socket.id);
+            console.log('Total clients', clientSet);
         })
     })
 }
