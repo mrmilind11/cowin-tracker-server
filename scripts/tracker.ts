d3bfca3b889c95118ffb64c4ae132486a80aad27
Milind Sharma <milind.sharma@searce.com>
2021-05-11 07:54:17 +0530

    Removed age limit

--- scripts/tracker.ts
+++ scripts/tracker.ts
@@ -2,7 +2,6 @@ import { forkJoin, Observable, of } from 'rxjs';
 import { IDistricts } from './../model/districts.interface';
 import { IVaccineCentre } from '../model/vaccine-centre.interface';
 import { CowinService } from './../service/cowin.service';
-import cron from 'node-cron';
 import nodenotifier from 'node-notifier'
 import { emitToSocket, onClientAdded, onClientRemoved } from './socket-server';
 import { map, switchMap, take } from 'rxjs/operators';
@@ -18,46 +17,6 @@ let centreList: IVaccineCentre[] = [];
 const clientSet = new Set();
 let dataFetchInProgress = false;
 let intervalRef: any;
-
-// const prepareCentreList = async () => {
-//     try {
-//         const allCentreData = await Promise.all(districtList.map((district) => CowinService.getCentreList(district.district_id, START_DATE)));
-//         const allCentreList: IVaccineCentre[] = allCentreData.reduce((current, value) => current.concat(value), []);
-//         console.log(allCentreList.length);
-//         const availableCentreList: IVaccineCentre[] = allCentreList.map((centre) => {
-//             const availableSessionList = centre.sessions.filter((session) => session.available_capacity > 0 && session.min_age_limit <= MIN_AGE);
-//             return {
-//                 ...centre,
-//                 sessions: availableSessionList
-//             }
-//         }).filter(({ sessions }) => sessions.length > 0);
-//         console.log('Available Centre', availableCentreList.length);
-//         // console.log('Available centre data', availableCentreList.map((centre) => ({
-//         //     center_id: centre.center_id,
-//         //     district_name: centre.district_name,
-//         //     block_name: centre.block_name,
-//         //     address: centre.address,
-//         //     fee_type: centre.fee_type,
-//         //     pincode: centre.pincode,
-//         //     session: centre.sessions.map(({ date, vaccine, available_capacity }) => ({ date, vaccine, available_capacity }))
-//         // })));
-//         // if (availableCentreList.length > 0) {
-//         //     availableCentreList.forEach((centre) => {
-//         //         const availableDetail = centre.sessions.reduce((current, value) => {
-//         //             return current + value.date + ' ' + value.available_capacity + '\n';
-//         //         }, '')
-//         //         nodenotifier.notify({
-//         //             title: centre.district_name + ' ' + centre.address,
-//         //             message: availableDetail,
-//         //             wait: false
-//         //         })
-//         //     })
-//         // }
-//         emitToSocket(AVAIL_DETAIL, availableCentreList);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
 
 const getDistrictList = (): Observable<IDistricts[]> => {
     if (districtList) {
@@ -77,12 +36,12 @@ const fetchCentreList = () => {
     dataFetchInProgress = true;
     const call$: Observable<IVaccineCentre[]> = getDistrictList()
         .pipe(switchMap((districts) => {
-            const districtWiseCenter$ = districtList.map((district) => CowinService.getCentreList(district.district_id, START_DATE));
+            const districtWiseCenter$ = districts.map((district) => CowinService.getCentreList(district.district_id, START_DATE));
             return forkJoin(districtWiseCenter$);
         }), map((response) => {
             const allCentreList = response.reduce((current, value) => current.concat(value), []);
             centreList = allCentreList.map((centre) => {
-                const availableSessionList = centre.sessions.filter((session) => session.available_capacity > 0 && session.min_age_limit <= MIN_AGE);
+                const availableSessionList = centre.sessions.filter((session) => session.available_capacity > 0);
                 return {
                     ...centre,
                     sessions: availableSessionList
@@ -141,10 +100,6 @@ const clientEventListener = () => {
 }
 
 const tracker = () => {
-    // cron.schedule('*/15 * * * * *', async () => {
-    //     await prepareCentreList();
-    // });
-    // task.stop();
     checkClientSize();
     clientEventListener();
 }
